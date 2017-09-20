dab9bf28e71281341b6a6d7f94e6747c7c188c48
David Cardoso <davidcardoso.ti@gmail.com>
2017-09-19 21:16:16 -0300

    - refactoring User model - updating .env.example with default admin user - added migration column role to user table - added migration create user admin

---
+++ database/migrations/2017_09_19_235058_create_user_admin_data.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use CodeFlix\Models\User;
+
+class CreateUserAdminData extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        User::create([
+            'name' => env('ADMIN_DEFAULT_NAME'),
+            'email' => env('ADMIN_DEFAULT_EMAIL'),
+            'password' => bcrypt(env('ADMIN_DEFAULT_PASSWORD')),
+            'role' => User::ROLE_ADMIN,
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        $user = User::where('email', '=', env('ADMIN_DEFAULT_EMAIL'))->first();
+
+        if ($user instanceof User) {
+            $user->delete();
+        }
+    }
+}
