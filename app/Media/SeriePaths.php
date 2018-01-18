a831a87ac991500647d6ebba449b2f062029a1e5
David Cardoso <davidcardoso.ti@gmail.com>
2018-01-18 19:26:01 -0300

    [WIP] CRUD Videos, thumbnail paths for Serie

--- app/Media/SeriePaths.php
+++ app/Media/SeriePaths.php
@@ -16,14 +16,41 @@ trait SeriePaths
     /**
      * @return string
      */
-    public function getThumbFolderStorageAttribute() { // mutator
+    public function getThumbFolderStorageAttribute(): string
+    { // mutator thumb_folder_storage
         return "series/{$this->id}";
     }
 
     /**
      * @return string
      */
-    public function getThumbRelativeAttribute() { // mutator
+    public function getThumbRelativeAttribute(): string
+    { // mutator thumb_relative
         return "{$this->thumb_folder_storage}/{$this->thumb}";
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getThumbPathAttribute()
+    { // mutator thumb_path
+        return $this->getAbsolutePath($this->getStorage(), $this->thumb_relative);
+    }
+
+    /**
+     * @return string
+     */
+    public function getThumbSmallRelativeAttribute(): string
+    { // mutator thumb_small_relative
+        [$name, $extension] = explode('.', $this->thumb);
+        return "{$this->thumb_folder_storage}/{$name}_small.{$extension}";
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getThumbSmallPathAttribute()
+    { // mutator thumb_small_path
+        return $this->getAbsolutePath($this->getStorage(), $this->thumb_small_relative);
     }
 }
