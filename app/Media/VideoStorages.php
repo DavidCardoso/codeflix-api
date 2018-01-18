7b2edb01d3df87d39453afc333ff6e0632208965
David Cardoso <davidcardoso.ti@gmail.com>
2018-01-18 19:26:01 -0300

    [WIP] CRUD Videos, thumbnail paths for Serie

--- app/Media/VideoStorages.php
+++ app/Media/VideoStorages.php
@@ -9,19 +9,33 @@
 namespace CodeFlix\Media;
 
 
+use Illuminate\Filesystem\FilesystemAdapter;
+
 trait VideoStorages
 {
     /**
      * @return \Illuminate\Filesystem\FilesystemAdapter
      */
-    public function getStorage() {
+    public function getStorage(): FilesystemAdapter
+    {
         return \Storage::disk($this->getDiskDriver());
     }
 
     /**
      * @return mixed
      */
-    protected function getDiskDriver() {
+    protected function getDiskDriver()
+    {
         return config('filesystems.default');
+    }
+
+    /**
+     * @param FilesystemAdapter $storage
+     * @param string $fileRelativePath
+     * @return mixed
+     */
+    protected function getAbsolutePath(FilesystemAdapter $storage, $fileRelativePath)
+    {
+        return $storage->getDriver()->getAdapter()->applyPathPrefix($fileRelativePath);
     }
 }
