d07238211ca8c54fe7d2aeb6c2b04bbd2a4845b6
David Cardoso <davidcardoso.ti@gmail.com>
2018-01-17 22:52:26 -0300

    [WIP] CRUD Videos, store relations

--- app/Http/Controllers/Admin/VideoRelationsController.php
+++ app/Http/Controllers/Admin/VideoRelationsController.php
@@ -4,11 +4,25 @@ namespace CodeFlix\Http\Controllers\Admin;
 
 use CodeFlix\Forms\VideoRelationForm;
 use CodeFlix\Models\Video;
+use CodeFlix\Repositories\VideoRepository;
 use Illuminate\Http\Request;
 use CodeFlix\Http\Controllers\Controller;
 
 class VideoRelationsController extends Controller
 {
+    /** @var VideoRepository $repository */
+    private $repository;
+
+    /**
+     * VideoRelationsController constructor.
+     * @param VideoRepository $repository
+     */
+    public function __construct(VideoRepository $repository)
+    {
+        $this->repository = $repository;
+    }
+
+
     /**
      * Show the form for creating a new resource.
      *
@@ -30,12 +44,28 @@ class VideoRelationsController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Http\Request $request
+     * @param integer $id
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(Request $request, $id)
     {
-        //
+        /** @var \FormBuilder $form */
+        $form = \FormBuilder::create(VideoRelationForm::class);
+
+        if (!$form->isValid()) {
+            return redirect()
+                ->back()
+                ->withErrors($form->getErrors())
+                ->withInput();
+        }
+
+        $data = $form->getFieldValues();
+        $this->repository->update($data, $id);
+        $request->session()->flash('success', 'Vinculos do video <b>atualizado</b> com sucesso!');
+
+        return redirect()->route('admin.videos.relations.create', ['video' => $id]);
+
     }
 
 
